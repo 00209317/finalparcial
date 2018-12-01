d950bf98cbd0e9ab618e3807759eca27a277bdbe
Daniel hernandez <00209317@uca.edu.sv>
2018-12-01 17:17:51 -0600

    modificando main js

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+//herbarios();
+document.forms.crud = addEventListener("submit", function(e){
+    e.preventDefault();
+    let data = {
+        nombre: document.querySelector('#nombre').value,
+        familia: document.querySelector('#familia').value,
+        anio: document.querySelector('#anio').value
+    }
+    let url = '/herbarios';
+    fetch(url,{
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            'content-type':'application/json'
+        }.then(res => res.JSON()).catch(error => res.JSON({error:error})).then(responde => {
+            alert("Insertado Correctamente");
+        })
+    })
+    
+});
